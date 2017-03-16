ee86358fa59d5744856d52af9bfb7cc3146a837c
Tom Armitage <tom@infovore.org>
2017-03-16 12:02:45 +0000

    Initial commit.

---
+++ elements.php
@@ -0,0 +1,70 @@
+<?php include("header.php"); ?>
+
+<div id="main">
+
+<h1>Elements</h1>
+<h2>Boxout</h2>
+
+<div class='boxout'>
+  <h2>Boxout heading</h2>
+  <div class='inner'>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua.
+    </p>
+    <p>
+      Duis aute irure <a href="#">dolor in reprehenderit</a> in voluptate velit esse
+      cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+      proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+    </p>
+    <ul>
+      <li><a href="#">thingy</a> thing thing zumbly thing.</li>
+      <li><a href="#">thingy</a> thing thing zumbly thing.</li>
+      <li><a href="#">thingy</a> thing thing zumbly thing.</li>
+      <li><a href="#">thingy</a> thing thing zumbly thing.</li>
+    </ul>
+  </div>
+</div>
+
+<h2>Boxout as sidebar</h2>
+
+<div class='boxout side'>
+  <h2>Boxout heading</h2>
+  <div class='inner'>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua.
+    </p>
+  </div>
+</div>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+consequat. Duis aute irure dolor in reprehenderit in voluptate velit.</p>
+
+<p>Esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+<div class="clear"><!-- clear --></div>
+
+<div class='boxout side right'>
+  <h2>Boxout heading</h2>
+  <div class='inner'>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua.
+    </p>
+  </div>
+</div>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+consequat. Duis aute irure dolor in reprehenderit in voluptate velit.</p>
+
+<p>Esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+<div class="clear"><!-- clear --></div>
+
+
+<?php include("footer.php"); ?>
---
+++ header.php
@@ -0,0 +1,9 @@
+<?php $is_project = false; ?>
+<html>
+<head>
+  <title>Foxfield Instruments</title>
+
+  <link rel="stylesheet" type="text/css" href="css/reset.css">
+  <link rel="stylesheet" type="text/css" href="css/gisborough.css">
+</head>
+<body>
