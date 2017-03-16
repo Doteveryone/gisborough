d5f702d4cac7028adbb8aa07f717ae75c82c608a
Tom Armitage <tom@infovore.org>
2017-03-16 12:06:03 +0000

    Simple menu for elements.

--- header.php
+++ header.php
@@ -7,3 +7,4 @@
   <link rel="stylesheet" type="text/css" href="css/gisborough.css">
 </head>
 <body>
+  <?php include('menu.php') ?>
---
+++ menu.php
@@ -0,0 +1,15 @@
+<style>
+  #gisborough-menu {
+    opacity: 0.8;
+    margin: 0 auto 20px;
+    width: 800px;
+    font-size: 90%;
+    padding-bottom: 3px;
+    border-bottom: 1px solid #eee;
+  }
+</style>
+<div id="gisborough-menu">
+  <b>Gisborough:</b>
+  <a href="markup.php">Markup &amp; Typography</a> | 
+  <a href="elements.php">Design elements</a>
+</div>
