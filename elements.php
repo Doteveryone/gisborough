c107bc789018fa063a086ce7ce5202fd331e5521
Tom Armitage <tom@infovore.org>
2017-03-16 12:54:23 +0000

    Better sample copy.

--- elements.php
+++ elements.php
@@ -8,15 +8,13 @@
 <div class='boxout'>
   <h2>Boxout heading</h2>
   <div class='inner'>
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-      tempor incididunt ut labore et dolore magna aliqua.
-    </p>
-    <p>
-      Duis aute irure <a href="#">dolor in reprehenderit</a> in voluptate velit esse
-      cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-      proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-    </p>
+    <p>"Just to amuse myself, and keep the good people busy, I ordered them to build this City, and my Palace; and they did it all willingly and well. Then I thought, as the country was so green and beautiful, I would call it the Emerald City; and to make the name fit better I put green spectacles on all the people, so that everything they saw was green."</p>
+
+<p>"But isn't everything here green?" asked Dorothy.</p>
+
+
+
+<p>"One of my greatest fears was the Witches, for while I had no magical powers at all I soon found out that the Witches were really able to do wonderful things.  There were four of them in this country, and they ruled the people who live in the North and South and East and West. Fortunately, the Witches of the North and South were good, and I knew they would do me no harm; but the Witches of the East and West were terribly wicked, and had they not thought I was more powerful than they themselves, they would surely have destroyed me.</p>  
     <ul>
       <li><a href="#">thingy</a> thing thing zumbly thing.</li>
       <li><a href="#">thingy</a> thing thing zumbly thing.</li>
@@ -31,19 +29,13 @@
 <div class='boxout side'>
   <h2>Boxout heading</h2>
   <div class='inner'>
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-      tempor incididunt ut labore et dolore magna aliqua.
-    </p>
+<p>"No more than in any other city," replied Oz; "but when you wear green spectacles, why of course everything you see looks green to you.  The Emerald City was built a great many years ago, for I was a young man when the balloon brought me here, and I am a very old man now.</p>  
   </div>
 </div>
-<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-consequat. Duis aute irure dolor in reprehenderit in voluptate velit.</p>
+<p>But my people have worn green glasses on their eyes so long that most of them think it really is an Emerald City, and it certainly is a beautiful place, abounding in jewels and precious metals, and every good thing that is needed to make one happy.  I have been good to the people, and they like me; but ever since this Palace was built, I have shut myself up and would not see any of them.</p>
+<p>As it was, I lived in deadly fear of them for many years; so you can imagine how pleased I was when I heard your house had fallen on the Wicked Witch of the East. When you came to me, I was willing to promise anything if you would only do away with the other Witch; but, now that you have melted her, I am ashamed to say that I cannot keep my promises."</p>
 
-<p>Esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+<p>"I think you are a very bad man," said Dorothy.</p>
 
 <div class="clear"><!-- clear --></div>
 
