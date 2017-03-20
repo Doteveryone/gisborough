391c3f3b45e37f6de8326d3aa805b02c44e6ca6c
Tom Armitage <tom@infovore.org>
2017-03-20 12:18:00 +0000

    Simple index.

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php include($_SERVER['DOCUMENT_ROOT']."/header.php"); ?>
+
+<div id="main">
+  <h1>Gisborough</h1>
+
+    <ul>
+      <li><a href="/markup.php">Markup demo</a></li>
+      <li><a href="/prototypes">Prototypes</a></li>
+    </ul>
+
+</div>
+<?php include($_SERVER['DOCUMENT_ROOT']."/footer.php"); ?>
