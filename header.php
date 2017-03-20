934d834f8b1886957a17a2260bb92eaf9649f602
Tom Armitage <tom@infovore.org>
2017-03-20 12:07:06 +0000

    Make hader use absolute paths to server route.

--- header.php
+++ header.php
@@ -3,10 +3,10 @@
 <head>
   <title>Gisborough</title>
 
-  <link rel="stylesheet" type="text/css" href="css/reset.css">
-  <link rel="stylesheet" type="text/css" href="css/gisborough.css">
+  <link rel="stylesheet" type="text/css" href="/css/reset.css">
+  <link rel="stylesheet" type="text/css" href="/css/gisborough.css">
   <script type="text/javascript" src="/js/jquery-3.1.1.min.js"></script>
   <script type="text/javascript" src="/js/jquery.scrollTo.min.js"></script>
 </head>
 <body>
-  <?php include('menu.php') ?>
+  <?php include($_SERVER['DOCUMENT_ROOT'].'/menu.php') ?>
