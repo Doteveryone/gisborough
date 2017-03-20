bb299e41f5909912f3f070179fa53751fd223a48
Tom Armitage <tom@infovore.org>
2017-03-20 17:34:04 +0000

    Incorporating Alex's feedback.

--- prototypes/upload.php
+++ prototypes/upload.php
@@ -11,15 +11,15 @@
 
 <div class='image-block'>
   <img src="images/upload/the-internet.png" />
-  <p class='caption'>The internet is a network of computers. TODO PLEASE CHANGE</p>
+  <p class='caption'>A simplified look at how your computer, tablet or phone connects with the vast internet network.</p>
 </div>
 
 
 <p>To be part of the internet, all devices (including yours) have to follow rules about sending information.</p>
 
 <p>
-  The rules for what format to send data in are called <abbr title="HyperText Transfer Protocol">HTTP</abbr> (hypertext transfer protocol).<br/>
-  The rules for where to send it and how are called <abbr title="Transmission Control Protocol / Internet Protocol">TCP/IP</abbr> (transmission control protocol / internet protocol).
+  The rules for what format to send data in are called <abbr title="HyperText Transfer Protocol">HTTP</abbr>.
+  The rules for where to send it and how are called <abbr title="Transmission Control Protocol / Internet Protocol">TCP/IP</abbr>.
 </p>
 
 <p>Here's what happens when you add a photo to the Weather Watchers page and click 'Post this report' &mdash; all within milliseconds.</p>
@@ -27,55 +27,60 @@
 <h2>What is sent?</h2>
 <p class='standfirst'>The most obvious data you send when you post your report is your image, your weather observations, and anything you write in the fields on the form.</p>
 
-<p>You're also sending other data you didn't directly type into the form, such as your login identity. On other sites, this other data might be the date and time you make a post, or the clicks you took to arrive on the page.</p>
+<p>You're also sending other data you didn't directly type into the form, such as your login identity, so your photo can be linked to your profile.</p>
+
+<p>On other sites, this data might be the date and time you make a post, or the clicks you took to arrive on the page.</p>
 
 <h2>Where is it sent?</h2>
-<p class='standfirst'>The data is sent to a particular <abbr title="Uniform Resource Locator">URL</abbr>. URLs are like addresses: they can represent the location of a page (like when you share a photo); they can also represent a location to send data to.</p>
+<p class='standfirst'>The data is sent to a particular <abbr title="Uniform Resource Locator">URL</abbr>. URLs are like addresses. They can represent the location of a page (like when you share a photo). They can also represent a location to send data to.</p>
 
 <p>The URL your data is sent to might look like this:</p> 
 
 <p><code>http://bbc.co.uk/weatherwatchers/send_report</code></p>
 
-<p>In that address, "<code>bbc.co.uk</code>" is the <em>domain</em> and <code>/weatherwatchers/send_report</code> is the path.</p>
+<p>In that address, "<code>bbc.co.uk</code>" is the domain and <code>/weatherwatchers/send_report</code> is the path.</p>
 
 <p>The domain is a friendly name for the computer we're sending it to. The path describes where on that computer to send it.</p>
 
 <p>To send your data, your device needs to know the domain's <abbr title="Internet Protocol">IP</abbr> (internet protocol) address. This is a unique string of numbers, and every device on the internet has one &mdash; including yours.</p>
 
-<p>Your device finds out the BBC's IP address by sending a request to a system that records all IP addresses, called the domain name system (<abbr title="Domain Name System">DNS</abbr>).</p>
+<p>Your device finds out the BBC's IP address by sending a request to the <abbr title='Domain Name System'>DNS</abbr> which records all IP addresses.</p>
 
 <p>Once the system's reply comes back, your device knows exactly where to send your data.</p>
 
 <h2>How is it sent?</h2>
-<p class='standfirst'>Now it's TCP/IP's turn. TCP/IP connects your device and the BBC's, so they can exchange data.</p>
-<p>TCP/IP chunks your data up into small packets and numbers them.</p>
+<p class='standfirst'>TCP/IP connects your device and the BBC's, so they can exchange data.</p>
+
+<p>TCP/IP also chunks your data up into lots of very small packets and numbers them.</p>
 
 <p>Your packets can take lots of possible routes through the internet to reach the BBC, so TCP/IP guarantees they are all delivered, and in the order they were sent.</p>
 
 <div style='width: 610px; margin: 0 auto 10px;' class='image-block'>
   <img src="images/upload/tcpip.png" />
-  <p class='caption'>TCP/IP in action.</p>
+  <p class='caption'>How TCP/IP sends your photo</p>
 </div>
 
 
 <h2>Arrival</h2>
 <p class='standfirst'>Your data packets arrive at the BBC's web server, which recombines them and uploads your data into the Weather Watchers software.</p>
 
-<p>The software stores your photo, weather observations, and account details in a structured way, so the Weather Watchers site can access and link it later. (For example, it can link your location data later to create a list of 'Photos near you' featuring your image.)</p>
+<p>The software stores your photo, weather observations, and account details in a structured way, so the Weather Watchers site can access and link it later.</p>
 
 <p>All of this activity happens in milliseconds!</p>
 
 <div class='boxout'>
-  <h2>When 'the internet breaks'</h2>
+  <h2>When the internet breaks</h2>
   <div class='inner'>
-    <p>If the internet is a network of devices, what happens when a link in that network breaks?</p>
 
-    <p>Often, not a lot: because there are many routes to the same direction, your data takes a different route.</p>
+    <p>If the internet is a network of devices, what happens when a connection in that network breaks?</p>
+    <p>Often, not a lot. There are so many connections, your data will just take a different route. This gives the internet resilience.</p>
+    <p>If a key connection in the network breaks, that can disrupt your access to the internet.</p>
+    <p>For instance, if you lose your internet connection at home, any one of the connections in the long chain might have broken – not necessarily the one leaving your house.</p>
+    <p>So if your connection breaks while downloading a web page or uploading a photo, it may well not be your fault!</p>
 
-    <p>Sometimes, though, if a key connection in the network breaks, than that can disrupt your access to the internet. If you've ever lost your internet connection at home, it might because of any one of the links - not necessarily the one leaving your house.</p>
-
-    <p>The number of possible connections makes the internet resilient - but if a key connection goes down that a country or company relies on, it can impact lots of people's access. If your connection breaks, it may well not be your fault!</p>
   </div>
+
+
 </div>
 
 </div>
