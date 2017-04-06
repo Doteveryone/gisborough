6e626a0c8408dde1f02b3909936cfc6bfccaba0b
Tom Armitage <tom@infovore.org>
2017-04-06 14:32:42 +0100

    First pass at privacy explainer.

---
+++ prototypes/privacy.php
@@ -0,0 +1,156 @@
+<?php include($_SERVER['DOCUMENT_ROOT']."/prototype_header.php"); ?>
+
+<div id="main">
+
+<div class='sticky-header'>
+  <div class='progbar'>
+  </div>
+  <h1>Security and Privacy</h1>
+</div>
+
+<p class='standfirst'>Security and privacy on the internet are sometimes thought of as being the same thing. In reality, they're quite different.</p>
+
+<p>Security is like a lock on a window of your house. You can decide to fit a lock, and who has a key. This controls <em>access</em> to your house.</p>
+
+<p>Privacy is like a curtain you hang at the window. You can choose when to open or close it, and what you're doing when it's open. The curtain controls what you keep <em>secret</em> in your house.</p>
+
+<p>Understanding this difference can help you see which elements of your internet presence you have control over, and how you can control your information.</p>
+
+<h2>Access security</h2>
+
+<p class='standfirst'>Access security is about personal security on the accounts, profiles and logins you have for websites or apps. There are three aspects: identity, authentication, and authorisation.</p>
+
+<h3>Identity: <em>who you are</em></h3>
+<p class='standfirst'>Different websites will ask for different combinations of identifying information to create an account identity. This might include email address, username or nickname, real name, postal address, and postcode.</p> 
+
+<p>For example, the Weather Watchers site asks you to create a nickname to display alongside your photo, rather than using your real name.</p>
+
+<p>Your identity is unique within each website. You can't usually have two accounts on the same site, which are tied to the same email address. But you might use the same identifying information on different websites.</p>
+
+<h3>Authentication: <em>you are who you say you are</em></h3>
+
+<p class='standfirst'>Before allowing access to your account, a website will often verify that you are who you say.</p>
+
+<p>You can think of this as the website providing the window lock, with you as the user bringing the key. If the key doesn't fit, you won't be able to enter.</p>
+
+<p>The most familiar method of authentication for websites is a password. Phones often have passcodes. Some devices now even use biometric identification, like thumb-print unlocking.</p>
+
+<p>Two-factor authentication adds an extra layer of verification. The person logging in needs to know the password and have access to a second device which is used to verify the password. Often it's a text message sent to a phone, or it's a dedicated device like a bank secure key.</p>
+
+<h3>Authorisation: <em>you're allowed to do the thing you want to do</em></h3>
+
+<p class='standfirst'>Authorisation allows or limits what you can do on a website or device once it has authenticated your identity.</p>
+
+<p>Some websites, like your bank's, won't let you do anything unless you're logged in.</p>
+
+<p>Other websites will let you do some things without being logged in, but allow more once you do. For example, anyone can browse and filter items on eBay, but only logged-in users can bid on them.</p>
+
+<div class='boxout'>
+<h2>Things that can go wrong</h2>
+  <div class='inner'>
+    <p><b>Identity:</b> Somebody might pretend to be you and make an account in your name. This is very rare, tending only to happen to people in the public eye.</p>
+    <p><b>Authentication:</b> Somebody could get hold of your password and access your identity. This is why you should use a different passwords on every account, and never write passwords down. A password manager like 1Pass or LastPass is really helpful here.</p>
+    <p><b>Authorisation:</b> You're able to do things on a site that you should be blocked from. This could be because there are bugs in the software that its engineers need to fix. Alternatively, someone might have given you too many permissions.</p>
+  </div>
+</div>
+
+<div class='boxout'>
+  <h2>Transmission security</h2>
+  <div class='inner'>
+    <p><img class='blkimg border' src="/prototypes/images/privacy/paypal-padlock.png" /></p>
+    <p>You might have noticed the green padlock symbol in your browser bar.</p>
+
+    <p>This symbol means that all of the computers that are part of the network leading to an end website, are sending data securely.</p>
+
+    <p>This symbol means that your data will be sent securely between your device and its destination. No computer that your data passes through en route will be able to see what you're sending - it's private between you and the endpoint</p>
+
+    <p>It doesn't mean that what you do on the website stays private, or that the end website stores your data securely.</p>
+
+    <p>All ecommerce websites have the green padlock because they ask buyers to send them credit card details. Never pay for anything on a website that doesn't have a green padlock.</p>
+  </div>
+</div>
+
+<h2>Privacy</h2>
+<p class='standfirst'>Privacy on the internet isn't something to automatically worry about – it's something to think about.</p>
+
+<p>As in your offline life, you decide what you're comfortable showing or talking about publicly online. Choices about privacy are very often concern what you want to keep secret, and who you want to keep it secret from.</p> 
+
+<p>These choices – about what you show, to whom, how, where and when – are personal and in your control. Once you make them, you can use settings on websites and apps to apply them to what you show and share.</p>
+
+<h2>Things to think about</h2>
+
+<h3>What you show</h3>
+
+<p class='standfirst'>How much are you comfortable with others knowing or seeing of your life – what you think and do, who you spend time with, where you go?</p>
+
+<p>Consider people: friends, family, acquaintances, teammates; colleagues, bosses, employees. Does what you share depend on, what you do, when you do it, or who you're with? Are there things you don't want to show or say of your free time, work time, family time?</p>
+
+<p>Consider events: weddings, birthdays and holidays; nights out and sports matches; conferences and leaving parties. Does what you keep private online match what you keep private offline? What different things do you do in different contexts that might alter what you share? What might people think about you sharing events that involve them?</p>
+
+<p>Consider topics: morals, ethics, religion, politics, personal experiences. Would you express the same views online to a wider audience as you would offline to a small group? Might you feel more open with a distant audience? What implications might expressing your views online have, beyond just making your point?</p>
+
+<h3>Who you show it to</h3>
+
+<p class='standfirst'>Do you want to limit which people can see what you think and do? If so, who do you feel comfortable seeing? Friends and family? Friends of your friends too? Anyone anywhere in the world?</p>
+
+<p>Perhaps who can see changes what you want to show – you have control over what you put out there.</p>
+
+<h3>How you show it</h3>
+
+<p class='standfirst'>You can choose how closely tied your real identity is to your online accounts.</p>
+
+<p>Are you using your real name? Are you completely anonymous? Are you using a pseudonym, like a nickname or handle? Can people work out who you are from it? Does that matter to you or in your wider life?</p>
+
+<h3>Where you show it</h3>
+
+<p class='standfirst'>All of the different sites and apps give you different functions and settings for showing information about yourself and your activities.</p>
+
+<p>Explore the options on the sites and apps you use or are interested in using. Which have more of the settings you want? Which are you more comfortable with for what purpose?</p>
+
+<p>Remember: you always have the option of not using a service – the easiest way to keep something private is to not share it anywhere.</p>
+
+<h3>When you show it</h3>
+
+<p class='standfirst'>Life changes over time, so time can also change your choices about your online privacy.</p>
+
+<p>Say you have children, take on a high-profile role, or move to a new town. You might choose to share more or less, or in a different way, depending on your social circle or work environment.</p>
+
+<p>It's a good idea to reconsider your privacy choices from time to time, and check you're still comfortable with what you're showing.</p>
+
+<div class='boxout'>
+  <h2>Privacy breaches</h2>
+  <div class='inner'>
+    <p>People often associate privacy breaches with deliberate attacks. But privacy is about people's choices, so privacy breaches are often down to human error.</p>
+
+    <h3>Mistakes</h3>
+
+    <p>A common mistake is being unaware of things being publicly visible. This is usually because a user hasn't switched the settings on a site or app to match what they want, giving more people authorisation to view things.</p>
+
+    <p><a href="https://identity.utexas.edu/everyone/how-to-manage-your-social-media-privacy-settings">How to check your settings on social media.</a></p>
+
+    <p>A common oversight is having one account identity inadvertently connect to another. For example, someone might use their real name and nickname on one site, but just a nickname on another. Googling could help someone link both accounts back to the real name.</p>
+
+    <p>Something commonly forgotten is what you share about other people. For example, if they're in photos with you, or you mention them in a post. Your choices can affect, and maybe breach, other people's privacy, just as theirs can yours. If you're in doubt, ask before you post.</p>
+
+    <h3>Lack of permission</h3>
+
+    <p>Other people's content isn't yours to use without their permission, and vice versa.</p>
+
+    <p>Anyone can link to a Weather Watcher's photo without permission, but it shouldn't be copied for another use without their permission.</p>
+
+    <p>Some sites and apps have terms and conditions on the use of members' content. Copyright law also applies, just as it does to offline creative outputs.</p>
+
+    <p>There are exceptions for <a href="https://en.wikipedia.org/wiki/Creative_Commons"Creative Commons</a> and public domain content. If you're in doubt, don't copy.</p>
+
+    <h3>Poor software</h3>
+
+    <p>Poorly designed software can make it unclear what's public and what's private. Settings and functions might be complicated to use, or it might be difficult to check what's visible to whom.</p>
+
+    <p>Badly made software could have bugs that unintentionally reveal information to the wrong users. These are often caught and fixed, but not always.</p>
+  </div>
+</div>
+
+</div>
+
+
+<?php include($_SERVER['DOCUMENT_ROOT']."/footer.php"); ?>
