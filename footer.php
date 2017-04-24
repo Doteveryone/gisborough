2735398a9b34056ff90305b9555a004f480f6716
Tom Armitage <tom@infovore.org>
2017-04-24 14:47:44 +0100

    If you liked it you'd stick a CC license on it.

--- footer.php
+++ footer.php
@@ -1,3 +1,8 @@
   <div class='clear'><!-- clear --></div>
+  <div id="footer">
+    <div class='inner'>
+      <a href="https://creativecommons.org/licenses/by-sa/4.0/legalcode"><abbr title='Creative Commons Attribution-ShareAlike 4.0 International license'>CC-BY-SA</abbr></a> DotEveryone 2017.
+    </div>
+  </div>
 </body>
 </html>
