b3038d8cac04c927ba05f0f186eec8805f54a736
Tom Armitage <tom@infovore.org>
2017-04-24 12:07:34 +0100

    Typo.

--- prototypes/privacy_security/security/problems.php
+++ prototypes/privacy_security/security/problems.php
@@ -10,7 +10,7 @@
 
 <p><b>Identity:</b> Somebody might pretend to be you and make an account in your name. This is very rare, tending only to happen to people in the public eye.</p>
 
-<p><b>Authentication:</b> Somebody could get hold of your password and access your identity. This is why you should use a different passwords on every account, and never write passwords down. A password manager like 1Pass or LastPass is really helpful here.</p>
+<p><b>Authentication:</b> Somebody could get hold of your password and access your identity. This is why you should use a different password on every account, and never write passwords down. A password manager like 1Pass or LastPass is really helpful here.</p>
 
 <p><b>Authorisation:</b> You're able to do things on a site that you should be blocked from. This could be because there are bugs in the software that its engineers need to fix. Alternatively, someone might have given you too many permissions.</p>
 
