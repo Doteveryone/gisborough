524a205a61a6619e8119cfa382e8d6b30950566c
Tom Armitage <tom@infovore.org>
2017-05-04 13:42:24 +0100

    Extracting routing to its own little file.

--- server.rb
+++ server.rb
@@ -1,47 +1,7 @@
 require 'sinatra'
+require './lib/routing'
 
-mappings = {"/" => {template: "index",
-                    title: nil},
-
-            "/sharing" => {template: "sharing",
-                           title: "What happens when I share my photo? : "},
-            "/upload" => {template: "upload",
-                           title: "What happens when I upload my photo? : "},
-            "/privacy_security" => {template: "privacy_security/index",
-                                    title: "Security and Privacy : "},
-            "/privacy_security/security" => {template: "privacy_security/security/index",
-                                             title: "Security : Security and Privacy : "},
-            "/privacy_security/security/identity" => {template: "privacy_security/security/identity",
-                                                      title: "Identity : Security : Security and Privacy : "},
-            "/privacy_security/security/authentication" => {template: "privacy_security/security/authentication",
-                                                            title: "Authentication : Security : Security and Privacy : "},
-            "/privacy_security/security/authorisation" => {template: "privacy_security/security/authorisation",
-                                                           title: "Authorisation : Security : Security and Privacy : "},
-            "/privacy_security/security/padlock" => {template: "privacy_security/security/padlock",
-                                                     title: "What the green padlock means : Security : Security and Privacy : "},
-            "/privacy_security/privacy" => {template: "privacy_security/privacy/index",
-                                            title: "Privacy : Security and Privacy : "},
-            "/privacy_security/privacy/what" => {template: "privacy_security/privacy/what",
-                                                 title: "What information you show : Privacy : Security and Privacy : "},
-            "/privacy_security/privacy/who" => {template: "privacy_security/privacy/who",
-                                                title: "Who you show information to : Privacy : Security and Privacy : "},
-            "/privacy_security/privacy/how" => {template: "privacy_security/privacy/how",
-                                                title: "How you show information : Privacy : Security and Privacy : "},
-            "/privacy_security/privacy/where" => {template: "privacy_security/privacy/where",
-                                                  title: "Where you show information : Privacy : Security and Privacy : "},
-             "/privacy_security/privacy/when" => {template: "privacy_security/privacy/when",
-                                                  title: "When you show information : Privacy : Security and Privacy : "},
-             "/privacy_security/privacy/breaches" => {template: "privacy_security/privacy/breaches/index",
-                                                      title: "Privacy breaches : Privacy : Security and Privacy : "},
-             "/privacy_security/privacy/breaches/revealing_too_much" => {template: "privacy_security/privacy/breaches/revealing_too_much",
-                                                                         title: "Revealing more than intended : Privacy breaches : Privacy : Security and Privacy : "},
-             "/privacy_security/privacy/breaches/permissions" => {template: "privacy_security/privacy/breaches/permissions",
-                                                                  title: "Taking without permission : Privacy breaches : Privacy : Security and Privacy : "},
-             "/privacy_security/privacy/breaches/software" => {template: "privacy_security/privacy/breaches/software",
-                                                               title: "Badly designed software : Privacy breaches : Privacy : Security and Privacy : "},
-}
-
-mappings.each do |route, options|
+MAPPINGS.each do |route, options|
   get route do
     erb options[:template].to_sym, layout: :main_layout, locals: {pagetitle: options[:title]}
   end
