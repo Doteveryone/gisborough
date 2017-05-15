e7ca1379adefd1e1ab94ee08e09b6a2e978beca1
Tom Armitage <tom@infovore.org>
2017-05-15 13:11:04 +0100

    Adding link to DotEveryone more clearly into the footer.

--- server.rb
+++ server.rb
@@ -1,8 +1,20 @@
 require 'sinatra'
 require './lib/routing'
 
+helpers do
+  def is_home
+    !locals[:title]
+  end
+end
+
+def is_home_for_path(path_string)
+  path_string.strip == "/"
+end
+
 MAPPINGS.each do |route, options|
   get route do
-    erb options[:template].to_sym, layout: :main_layout, locals: {pagetitle: options[:title]}
+    is_home = is_home_for_path(route)
+
+    erb options[:template].to_sym, layout: :main_layout, locals: {pagetitle: options[:title], is_home: is_home}
   end
 end
